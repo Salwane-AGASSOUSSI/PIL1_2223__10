22c2784ef049755f5aa3ef220d35d4c084f7f0aa
Salwane <agassoussisalwane2@gmail.com>
2023-06-15 08:30:24 +0100

    remise en ordre

--- Dossier_back/projects/web-app/timebase/timebase/asgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('settings.py', 'settings.py')
-
-application = get_asgi_application()
--- Dossier_back/projects/web-app/timebase/timebase/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('settings.py', 'settings.py')
-
-application = get_wsgi_application()
-
