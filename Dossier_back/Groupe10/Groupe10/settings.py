ed8e0c5ad4ed7b2529e89f57e719f899e4c034a6
Celi003 <123420334+Celi003@users.noreply.github.com>
2023-06-20 20:54:48 +0100

    Update settings.py
    J'ai ajouté le code approprier

--- Dossier_back/Groupe10/Groupe10/settings.py
+++ Dossier_back/Groupe10/Groupe10/settings.py
@@ -118,6 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your-email@gmail.com'  # Remplacez par votre adresse e-mail Gmail
+EMAIL_HOST_PASSWORD = 'your-password'  # Remplacez par votre mot de passe Gmail
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'your-email@gmail.com'  # Remplacez par votre adresse e-mail Gmail
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
